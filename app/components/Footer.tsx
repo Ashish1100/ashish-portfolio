86d699162bbf0db1328f7a80773198ad3692a92b
Ashish1100 <ashishsaha224466@gmail.com>
2025-02-06 21:41:10 +0530

    copyright

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
   return (
     <div className="max-w-6xl mx-auto flex flex-col  px-4 py-8 text-center text-xs opacity-40">
-      {`© Copyright Ashish Saha ${getCurrentYear}. All Rights are Reserved.`}
+      {`© Copyright Ashish Saha ${getCurrentYear}. All Rights Are Reserved.`}
     </div>
   );
 };
